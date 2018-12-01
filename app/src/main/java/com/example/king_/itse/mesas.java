2460d63aaeab715cc7b475dd38ff0f5789cbd880
IIXIIGuillermoIIXII <42657956+iixiiguillermoiixii@users.noreply.github.com>
2018-12-01 16:46:50 -0600

    activity de mesas

--- app/src/main/java/com/example/king_/itse/mesas.java
+++ app/src/main/java/com/example/king_/itse/mesas.java
@@ -3,12 +3,11 @@ package com.example.king_.itse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class mesas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_mesas);
     }
 }
